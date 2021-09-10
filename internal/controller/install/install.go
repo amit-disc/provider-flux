ebe40e894cee0473690b7cf05b6c18f8eac3785a
Amit Agarwal <amit_agarwal@discovery.com>
2021-09-10 16:18:57 +0200

    initial commit

---
+++ internal/controller/install/install.go
@@ -0,0 +1,289 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package install
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+
+	"github.com/crossplane/crossplane-runtime/pkg/event"
+	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
+	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+
+	"github.com/crossplane/provider-flux/apis/sample/v1alpha1"
+	apisv1alpha1 "github.com/crossplane/provider-flux/apis/v1alpha1"
+	"github.com/amit-disc/provider-flux/internal/clients"
+	"github.com/fluxcd/flux2/pkg/manifestgen/install"
+)
+
+const (
+	errNotMyType    = "managed resource is not a Install custom resource"
+	errTrackPCUsage = "cannot track ProviderConfig usage"
+	errGetPC        = "cannot get ProviderConfig"
+	errGetCreds     = "cannot get credentials"
+
+	errNewClient = "cannot create new Service"
+)
+
+// Setup adds a controller that reconciles MyType managed resources.
+func Setup(mgr ctrl.Manager, l logging.Logger, rl workqueue.RateLimiter) error {
+	name := managed.ControllerName(v1alpha1.MyTypeGroupKind)
+
+	o := controller.Options{
+		RateLimiter: ratelimiter.NewDefaultManagedRateLimiter(rl),
+	}
+
+	r := managed.NewReconciler(mgr,
+		resource.ManagedKind(v1alpha1.MyTypeGroupVersionKind),
+		managed.WithExternalConnecter(&connector{
+			kube:         mgr.GetClient(),
+			usage:        resource.NewProviderConfigUsageTracker(mgr.GetClient(), &apisv1alpha1.ProviderConfigUsage{}),
+			newRestConfigFn: clients.NewRestConfig,
+			newKubeClientFn: clients.NewKubeClient,
+		managed.WithLogger(l.WithValues("controller", name)),
+		managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))))
+
+	return ctrl.NewControllerManagedBy(mgr).
+		Named(name).
+		WithOptions(o).
+		For(&v1alpha1.MyType{}).
+		Complete(r)
+}
+
+// A connector is expected to produce an ExternalClient when its Connect method
+// is called.
+type connector struct {
+	kube         client.Client
+	usage        resource.Tracker
+	newServiceFn func(creds []byte) (interface{}, error)
+	newRestConfigFn func(kubeconfig []byte) (*rest.Config, error)
+	newKubeClientFn func(config *rest.Config) (client.Client, error)
+}
+
+func (c *connector) Connect(ctx context.Context, mg resource.Managed) (managed.ExternalClient, error) {
+	cr, ok := mg.(*v1alpha1.Install)
+	if !ok {
+		return nil, errors.New(errNotKubernetesObject)
+	}
+
+	if err := c.usage.Track(ctx, mg); err != nil {
+		return nil, errors.Wrap(err, errTrackPCUsage)
+	}
+
+	pc := &apisv1alpha1.ProviderConfig{}
+	if err := c.kube.Get(ctx, types.NamespacedName{Name: cr.GetProviderConfigReference().Name}, pc); err != nil {
+		return nil, errors.Wrap(err, errGetPC)
+	}
+
+	var rc *rest.Config
+	var err error
+	cd := pc.Spec.Credentials
+
+	if cd.Source == xpv1.CredentialsSourceInjectedIdentity {
+		rc, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, errors.Wrap(err, errFailedToCreateRestConfig)
+		}
+	} else {
+		var kc []byte
+		if kc, err = resource.CommonCredentialExtractor(ctx, cd.Source, c.kube, cd.CommonCredentialSelectors); err != nil {
+			return nil, errors.Wrap(err, errGetCreds)
+		}
+
+		if rc, err = c.newRestConfigFn(kc); err != nil {
+			return nil, errors.Wrap(err, errFailedToCreateRestConfig)
+		}
+	}
+
+	k, err := c.newKubeClientFn(rc)
+	if err != nil {
+		return nil, errors.Wrap(err, errNewKubernetesClient)
+	}
+
+	return &external{
+		logger: c.logger,
+		client: resource.ClientApplicator{
+			Client:     k,
+			Applicator: resource.NewAPIPatchingApplicator(k),
+		},
+	}, nil
+}
+
+type external struct {
+	logger logging.Logger
+	client resource.ClientApplicator
+}
+
+func (c *external) Observe(ctx context.Context, mg resource.Managed) (managed.ExternalObservation, error) {
+	cr, ok := mg.(*v1alpha1.Install)
+	if !ok {
+		return managed.ExternalObservation{}, errors.New(errNotKubernetesObject)
+	}
+
+	c.logger.Debug("Observing", "resource", cr)
+
+	desired, err := getDesired(cr)
+	if err != nil {
+		return managed.ExternalObservation{}, err
+	}
+
+	observed := desired.DeepCopy()
+
+	err = c.client.Get(ctx, types.NamespacedName{
+		Namespace: observed.GetNamespace(),
+		Name:      observed.GetName(),
+	}, observed)
+
+	if kerrors.IsNotFound(err) {
+		return managed.ExternalObservation{ResourceExists: false}, nil
+	}
+	if err != nil {
+		return managed.ExternalObservation{}, errors.Wrap(err, errGetInstall)
+	}
+
+	if err = setObserved(cr, observed); err != nil {
+		return managed.ExternalObservation{}, err
+	}
+
+	var last *unstructured.Unstructured
+	if last, err = getLastApplied(cr, observed); err != nil {
+		return managed.ExternalObservation{}, errors.Wrap(err, errGetLastApplied)
+	}
+	if last == nil {
+		return managed.ExternalObservation{
+			ResourceExists:   true,
+			ResourceUpToDate: false,
+		}, nil
+	}
+
+	if equality.Semantic.DeepEqual(last, desired) {
+		c.logger.Debug("Up to date!")
+		return managed.ExternalObservation{
+			ResourceExists:   true,
+			ResourceUpToDate: true,
+		}, nil
+	}
+
+	return managed.ExternalObservation{
+		ResourceExists:   true,
+		ResourceUpToDate: false,
+	}, nil
+}
+
+func (c *external) Create(ctx context.Context, mg resource.Managed) (managed.ExternalCreation, error) {
+	cr, ok := mg.(*v1alpha1.Install)
+	flux_manifests := generateManifests()
+	if !ok {
+		return managed.ExternalCreation{}, errors.New(errNotKubernetesObject)
+	}
+
+	c.logger.Debug("Creating", "resource", cr)
+	obj, err := getDesired(cr)
+	if err != nil {
+		return managed.ExternalCreation{}, err
+	}
+
+	meta.AddAnnotations(obj, map[string]string{
+		v1.LastAppliedConfigAnnotation: string(flux_manifests),
+	})
+
+	if err := c.client.Create(ctx, obj); err != nil {
+		return managed.ExternalCreation{}, errors.Wrap(err, errCreateInstall)
+	}
+
+	cr.Status.SetConditions(xpv1.Available())
+	return managed.ExternalCreation{}, setObserved(cr, obj)
+}
+
+func (c *external) Update(ctx context.Context, mg resource.Managed) (managed.ExternalUpdate, error) {
+	cr, ok := mg.(*v1alpha1.Install)
+	flux_manifests := generateManifests()
+	if !ok {
+		return managed.ExternalUpdate{}, errors.New(errNotKubernetesObject)
+	}
+
+	c.logger.Debug("Updating", "resource", cr)
+
+	obj, err := getDesired(cr)
+	if err != nil {
+		return managed.ExternalUpdate{}, err
+	}
+
+	meta.AddAnnotations(obj, map[string]string{
+		v1.LastAppliedConfigAnnotation: string(flux_manifests),
+	})
+
+	if err := c.client.Apply(ctx, obj); err != nil {
+		return managed.ExternalUpdate{}, errors.Wrap(err, errApplyInstall)
+	}
+
+	cr.Status.SetConditions(xpv1.Available())
+	return managed.ExternalUpdate{}, setObserved(cr, obj)
+}
+
+func (c *external) Delete(ctx context.Context, mg resource.Managed) error {
+	cr, ok := mg.(*v1alpha1.Install)
+	if !ok {
+		return errors.New(errNotKubernetesObject)
+	}
+
+	c.logger.Debug("Deleting", "resource", cr)
+	obj, err := getDesired(cr)
+	if err != nil {
+		return err
+	}
+
+	return errors.Wrap(resource.IgnoreNotFound(c.client.Delete(ctx, obj)), errDeleteInstall)
+}
+
+
+func getLastApplied(obj *v1alpha1.Install, observed *unstructured.Unstructured) (*unstructured.Unstructured, error) {
+	lastApplied, ok := observed.GetAnnotations()[v1.LastAppliedConfigAnnotation]
+	if !ok {
+		return nil, nil
+	}
+
+	last := &unstructured.Unstructured{}
+	if err := json.Unmarshal([]byte(lastApplied), last); err != nil {
+		return nil, errors.Wrap(err, errUnmarshalTemplate)
+	}
+
+	if last.GetName() == "" {
+		last.SetName(obj.Name)
+	}
+
+	return last, nil
+}
+
+
+func generateManifests(mg resource.Managed) string {
+	cr, ok := mg.(*v1alpha1.Install)
+	opt := install.MakeDefaultOptions()
+	opt.Version = string(cr.Spec.ForProvider.Version))
+	opt.Namespace = string(cr.Spec.ForProvider.Namespace))	
+	manifest, _ := install.Generate(opt, "")
+	return manifest.Content
+}
